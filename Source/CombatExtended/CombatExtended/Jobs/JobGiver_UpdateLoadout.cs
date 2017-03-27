c2b423a5aecb8bc6abd0eb6d91b84cb170e38970
ProfoundDarkness <prodark77@gmail.com>
2017-03-27 15:07:42 -0700

    All CEVerb weapons that use ammo should do a reload before warmup.
    Took some time to find the interesting point where a pawn switches from trying to shoot to the warmup but finally found it.  From there did a Harmony Hybrid Infixed Prefix (functions like a Prefix but is Infixed).
    
    The infix patch passes the instance verb into a needs reload check.  If the check returns false that means there was work to do (yeah, backwards) and the pawn shouldn't enter the warmup stance.  If true then either it wasn't a gun with ammo or it didn't need reload, the pawn enters the reload stance.
    
    Thanks to whoever created/maintained the CompAmmoUser.TryStartReload() as I had no idea how to get turrets reloading and that just handled it for me.
    
    Also includes some side fixes related to loadouts.

--- Source/CombatExtended/CombatExtended/Jobs/JobGiver_UpdateLoadout.cs
+++ Source/CombatExtended/CombatExtended/Jobs/JobGiver_UpdateLoadout.cs
@@ -94,7 +94,7 @@ namespace CombatExtended
 						{
 							if (listing.ContainsKey(curSlot.thingDef))
 							{
-								int amount = listing[curSlot.thingDef].value >= wantCount ? wantCount : wantCount - listing[curSlot.thingDef].value;
+								int amount = listing[curSlot.thingDef].value >= wantCount ? wantCount : listing[curSlot.thingDef].value;
 								listing[curSlot.thingDef].value -= amount;
 								wantCount -= amount;
 								if (listing[curSlot.thingDef].value <= 0)
@@ -107,7 +107,7 @@ namespace CombatExtended
 							int amount;
 							foreach (ThingDef def in listing.Keys.Where(td => curSlot.genericDef.lambda(td)))
 							{
-								amount = listing[def].value >= wantCount ? wantCount : wantCount - listing[def].value;
+								amount = listing[def].value >= wantCount ? wantCount : listing[def].value;
 								listing[def].value -= amount;
 								wantCount -= amount;
 								if (listing[def].value <= 0)
@@ -281,13 +281,11 @@ namespace CombatExtended
 	                    {
 	                        return new Job(JobDefOf.Equip, closestThing);
 	                    }
-	                    // Take items into inventory if needed
-	                    int numContained = inventory.container.TotalStackCountOfDef(closestThing.def);
-	                    return new Job(JobDefOf.TakeInventory, closestThing) { count = Mathf.Min(closestThing.stackCount, count - numContained) };
+	                    return new Job(JobDefOf.TakeInventory, closestThing) { count = Mathf.Min(closestThing.stackCount, count) };
 	                } else
 	                {
 	                	return new Job(CE_JobDefOf.TakeFromOther, closestThing, carriedBy, doEquip ? pawn : null) {
-	                		count = doEquip ? 1 : Mathf.Min(closestThing.stackCount, count - inventory.container.TotalStackCountOfDef(closestThing.def))
+	                		count = doEquip ? 1 : Mathf.Min(closestThing.stackCount, count)
 	                	};
 	                }
                 }
