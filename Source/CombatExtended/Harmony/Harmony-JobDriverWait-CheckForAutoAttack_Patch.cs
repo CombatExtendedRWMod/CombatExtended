6954b0a806c20ded0707371bc798c83a62643cf1
ProfoundDarkness <prodark77@gmail.com>
2017-04-20 06:19:58 -0700

    Reverted Verb_LaunchProjectileCE, Cleaned up new Harmony patch.
    Used git to view an older version of Verb_LaunchProjectileCE.GetPartialCoverBetween() and undid the change I made to add a static call to that method.
    
    Also removed the old harmony patch.  While interesting it's no longer useful.
    
    Cleaned up Harmony-JobDriverWait-CheckForAutoAttack_Patch.cs as I was all over the map trying to figure out how to store a new local variable when it turns out all I needed was to replace an ldnull with an ldloc_s <verb> and a call to a method to generate the predicate.

--- Source/CombatExtended/Harmony/Harmony-JobDriverWait-CheckForAutoAttack_Patch.cs
+++ Source/CombatExtended/Harmony/Harmony-JobDriverWait-CheckForAutoAttack_Patch.cs
@@ -9,20 +9,23 @@ using Harmony.ILCopying;
 using Verse;
 using Verse.AI;
 
+/*
+ * Targetting the Verse.AI.JobDriver_Wait.CheckForAutoAttack()
+ * Target Line:
+ *  Thing thing = AttackTargetFinder.BestShootTargetFromCurrentPosition(this.pawn, null, verb.verbProps.range, verb.verbProps.minRange, targetScanFlag);
+ *  
+ * Basically modify that line to read something like:
+ *  Thing thing = AttackTargetFinder.BestShootTargetFromCurrentPosition(this.pawn, GetValidTargetPredicate(verb), verb.verbProps.range, verb.verbProps.minRange, targetScanFlag);
+ * 
+ * Overall does a couple of things.  First it locates the local variable with the verb used to attack with and second it locates the null argument in the above method call
+ * for Predicate and replaces that with an arg stack load of the verb and a call to create a predicate.  That call removes the verb from the call stack and replaces it
+ * with a predicate (or a null).
+ * 
+ * A couple of helper functions to turn a bunch of ifs into a single call since IL can use one of 6 instructions for local variable load/save.
+ */
+
 namespace CombatExtended.Harmony
 {
-    /*
-     * Targetting the Verse.AI.JobDriver_Wait.CheckForAutoAttack()
-     * Target Line:
-     *  Thing thing = AttackTargetFinder.BestShootTargetFromCurrentPosition(this.pawn, null, verb.verbProps.range, verb.verbProps.minRange, targetScanFlag);
-     *  
-     * Basically modify that line to read something like:
-     *  Predicate<Thing> predicate = GetValidTargetPredicate(verb)
-     *  Thing thing = AttackTargetFinder.BestShootTargetFromCurrentPosition(this.pawn, predicate, verb.verbProps.range, verb.verbProps.minRange, targetScanFlag);
-     */
-
-    // (ProfoundDarkness) btw: I don't know how to insert a new local variable just yet...  The rest should be pretty easy.
-
     [HarmonyPatch(typeof(JobDriver_Wait))]
     [HarmonyPatch("CheckForAutoAttack")]
     static class Harmony_JobDriverWait_CheckForAutoAttack_Patch
@@ -30,30 +33,23 @@ namespace CombatExtended.Harmony
         static readonly string logPrefix = Assembly.GetExecutingAssembly().GetName().Name + " :: " + typeof(Harmony_JobDriverWait_CheckForAutoAttack_Patch).Name + " :: ";
         static DynamicMethod Patched_ClosestThingTarget_Global = null;
 
+        /// <summary>
+        /// Transpiler runs through the IL code of the method CheckForAutoAttack and makes some tweaks to a call so as to avoid having the pawn attack a target it can't hit.
+        /// </summary>
+        /// <param name="instructions">IEnumerable of CodeInstruction, required by Harmony, the IL code Harmony fetched/uses.</param>
+        /// <returns>IEnumerable of CodeInstruction containing the changes to the method's IL code.</returns>
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            /* (ProfoundDarkness) Rough outline...
-             * First find what the highest local variable index is (current code base is 12 but that might change).
-             * Also while finding that locate the call to BestShootTargetFromCurrentPosition().
-             * When found need to search backwards for an LdNull opcode as well as a label.
-             * The ldNull is where our new local variable goes (replacement) and right after the label is where we insert our call to get the predicate.
-             * 
-             * 
-             * Just realized... it's an argument stack so should be able to just load the verb on and then make the call.
-             * The call should consume 1 argument and then leave a return value on the arg stack...
-             */
-
             int verbLocalIndex = -1;
             int indexKeyCall = 0;
 
-            // turn instructions into a list so we can walk through it variably (instead of linear only).
-
+            // turn instructions into a list so we can walk through it variably (instead of forward only).
             List<CodeInstruction> code = instructions.ToList();
 
             // walk forward to find some key information.
             for (int i = 0; i < code.Count(); i++)
             {
-                // look for the verb instanciation/storage.
+                // look for the verb instantiation/storage.
                 {
                     MethodBase method = null;
                     if (code[i].opcode == OpCodes.Callvirt && (method = code[i].operand as MethodBase) != null && method.DeclaringType == typeof(Pawn) && method.Name == "TryGetAttackVerb"
@@ -74,7 +70,7 @@ namespace CombatExtended.Harmony
             }
 
             Log.Message(string.Concat("verbLocalIndex ", verbLocalIndex));
-            // walk backwards from the key call to locate the null load and replace it with our call.
+            // walk backwards from the key call to locate the null load and replace it with our call to drop in our predicate into the arg stack.
             for (int i = indexKeyCall; i >= 0; i--)
             {
                 if (code[i].opcode == OpCodes.Ldnull)
@@ -87,86 +83,7 @@ namespace CombatExtended.Harmony
                 }
             }
 
-            //for (int i = 0; i < code.Count(); i++)
-            //    Log.Message(string.Concat(code[i].opcode, " - type(", (code[i].operand == null ? "null" : code[i].GetType().ToString()), ") = ", code[i].operand));
-
-
-            //// old
-            //List<HarmonyMethod> methods = typeof(JobDriver_Wait).GetHarmonyMethods();
-            //HarmonyMethod method = methods.First<HarmonyMethod>();
-            //(method.method as DynamicMethod).GetILGenerator();
-            //(method.method as MethodBase).GetILGenerator();
-
-            // walk forward to find some key information.
-            //for (int i = 0; i < code.Count(); i++)
-            //{
-            // update the largest found local variable index.
-            //    int tmp = 0;
-            //if ((tmp = OpcodeStoreIndex(code[i])) >= 0 && tmp > largestLocalIndex)
-            //    largestLocalIndex = tmp;
-
-            // try to find the verb index...
-
-
-            // see if we've found the instruction index of the key call.
-            //    if (code[i].opcode == OpCodes.Call && (code[i].operand as MethodInfo) != null)
-            //    {
-            //MethodInfo method = code[i].operand as MethodInfo;
-            //        if (method.DeclaringType == typeof(AttackTargetFinder) && method.Name == "BestShootTargetFromCurrentPosition")
-            //            indexKeyCall = i;
-            //    }
-            //}
-
-            // walk backwards from the key call to locate the null (replacement) and the label (insertion point)
-            //for (int i = indexKeyCall; i >= 0; i--)
-            //{
-            // replacement detection.  Want to replace the null with load of our predicate.
-            //if (code[i].opcode == OpCodes.Ldnull)
-            //    code[i] = MakeLocalLoadInstruction(largestLocalIndex + 1);
-
-            // insertion detection.  Want to insert after the label (looking upwards from key method). (label is metadata so ldarg.0...)
-            //if (code[i].opcode == OpCodes.Ldarg_0)
-            //{
-            //code.Insert(i, new CodeInstruction)
-            //code.Insert(i, new CodeInstruction(OpCodes.Call, ));
-            //}
-            //}
-
             return code;
-        }
-
-        /// <summary>
-        /// Creates a store instruction to store a variable from the evaluation stack.
-        /// </summary>
-        /// <param name="index">index that the variable is supposed to go to.</param>
-        /// <param name="storageType">Type of the object to store.</param>
-        /// <returns>CodeInstruction with the correct opcode and possibly operand of the object to be stored.</returns>
-        static CodeInstruction MakeLocalStoreInstruction(int index, Type storageType)
-        {
-            //DynamicMethod tmeth = new DynamicMethod("garbage", null, null);
-            //ILGenerator ilGen = tmeth.GetILGenerator();
-            //LocalBuilder var = ilGen.DeclareLocal(typeof(Predicate<Thing>));
-
-            // argument check...
-            if (index < 0 || index > UInt16.MaxValue)
-                throw new ArgumentException("Index must be greater than 0 and less than " + uint.MaxValue.ToString() + ".");
-            
-            // the first 4 are easy...
-            switch (index)
-            {
-                case 0:
-                    return new CodeInstruction(OpCodes.Stloc_0);
-                case 1:
-                    return new CodeInstruction(OpCodes.Stloc_1);
-                case 2:
-                    return new CodeInstruction(OpCodes.Stloc_2);
-                case 3:
-                    return new CodeInstruction(OpCodes.Stloc_3);
-            }
-
-            // proper type info for the other items.
-            if (index > Byte.MaxValue) return new CodeInstruction(OpCodes.Stloc_S, storageType);
-            return new CodeInstruction(OpCodes.Stloc, storageType);
         }
 
         /// <summary>
@@ -223,14 +140,14 @@ namespace CombatExtended.Harmony
         /// <returns>Predicate of type Thing which indicates if that thing is a valid target for the pawn.</returns>
         static Predicate<Thing> GetValidTargetPredicate(Verb verb)
         {
-            Predicate<Thing> pred = null;
+            Predicate<Thing> predicate = null;
             if ((verb as Verb_LaunchProjectileCE) != null)
             {
                 Verb_LaunchProjectileCE verbCE = verb as Verb_LaunchProjectileCE;
-                pred = t => verbCE.CanHitTargetFrom(verb.caster.Position, new LocalTargetInfo(t));
+                predicate = t => verbCE.CanHitTargetFrom(verb.caster.Position, new LocalTargetInfo(t));
             }
 
-            return pred;
+            return predicate;
         }
     }
 }
