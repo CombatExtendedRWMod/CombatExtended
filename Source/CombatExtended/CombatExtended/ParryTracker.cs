7637497dc3f33038c7bff93f733ed0a9a87da643
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2017-04-30 14:44:37 +0200

    Started work on parry system
    - Partially implemented Parry Tracker (still needs ExposeData implemented)
    - Created Harmony prefix for Verb_MeleeAttack.TryCastShot()
    - Added Critical hit chance stat
    - Fixed bug where suppression cover search wouldn't consider pawn's current region
    - Added LoS checks to suppression cover search, will now prefer locations without LoS to enemy that pawn has direct LoS to
    - Added preliminary implementation of flak vest
    - Fixed faulty turret recipes
    - Fixed heavy auto-turret research having wrong prerequisite
    - Fixed issue with mortars throwing errors because of faulty charge settings

---
+++ Source/CombatExtended/CombatExtended/ParryTracker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace CombatExtended
+{
+    public class ParryTracker : MapComponent
+    {
+        private struct ParryCounter
+        {
+            public int parries;
+            public int ticks;
+
+            public static ParryCounter MakeNew()
+            {
+                return new ParryCounter { parries = 0, ticks = Find.TickManager.TicksGame };
+            }
+        }
+
+        private const int SkillPerParry = 4;    // Award another parry per this many skill levels
+        private const int TicksToTimeout = 120; // Reset parry counter after this many ticks
+
+        private Dictionary<Pawn, ParryCounter> parryTracker = new Dictionary<Pawn, ParryCounter>();
+
+        public ParryTracker(Map map) : base(map)
+        {
+        }
+
+        private int GetUsedParriesFor(Pawn pawn)
+        {
+            ParryCounter counter;
+            if (!parryTracker.TryGetValue(pawn, out counter))
+            {
+                return 0;
+            }
+            return counter.parries;
+        }
+
+        public bool CheckCanParry(Pawn pawn)
+        {
+            if (pawn == null)
+            {
+                Log.Error("CE tried checking CanParry with Null-Pawn");
+                return false;
+            }
+
+            // Check if our target is immobile
+            if (!pawn.RaceProps.Humanlike || pawn.Downed || pawn.GetPosture() != PawnPosture.Standing || pawn.stances.stunner.Stunned || pawn.story.WorkTagIsDisabled(WorkTags.Violent))
+            {
+                return false;
+            }
+
+            int parriesLeft = Mathf.RoundToInt(pawn.skills.GetSkill(SkillDefOf.Melee).Level / SkillPerParry) - GetUsedParriesFor(pawn);
+            return parriesLeft > 0;
+        }
+
+        public void RegisterParryFor(Pawn pawn)
+        {
+            ParryCounter counter;
+            if (!parryTracker.TryGetValue(pawn, out counter))
+            {
+                // Register new pawn in tracker
+                counter = ParryCounter.MakeNew();
+                parryTracker.Add(pawn, counter);
+            }
+            counter.parries++;
+        }
+
+        public void ResetParriesFor(Pawn pawn)
+        {
+            parryTracker.Remove(pawn);
+        }
+
+        public override void MapComponentTick()
+        {
+            if (Find.TickManager.TicksGame % 10 == 0)
+            {
+                foreach (var entry in parryTracker.Where(kvp => Find.TickManager.TicksGame - kvp.Value.ticks >= TicksToTimeout).ToArray())
+                {
+                    parryTracker.Remove(entry.Key);
+                }
+            }
+        }
+
+        public override void ExposeData()
+        {
+            // TODO Save parryTracker
+        }
+    }
+}
---
+++ Source/CombatExtended/Harmony/Harmony-Verb_MeleeAttack-TryCastShot_Patch.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.Sound;
+using UnityEngine;
+using Harmony;
+
+namespace CombatExtended.Harmony
+{
+    [HarmonyPatch(typeof(Verb_MeleeAttack))]
+    [HarmonyPatch("TryCastShot")]
+    static class Harmony_Verb_MeleeAttack_TryCastShot_Patch
+    {
+        public static bool Prefix(Verb_MeleeAttack __instance, ref bool __result)
+        {
+            var verb_MeleeAttack = Traverse.Create(__instance);
+
+            Pawn casterPawn = __instance.CasterPawn;
+            if (casterPawn.stances.FullBodyBusy)
+            {
+                return false;
+            }
+            LocalTargetInfo currentTarget = verb_MeleeAttack.Field("currentTarget").GetValue<LocalTargetInfo>();
+            Thing thing = currentTarget.Thing;
+            if (!__instance.CanHitTarget(thing))
+            {
+                Log.Warning(string.Concat(new object[]
+                {
+            casterPawn,
+            " meleed ",
+            thing,
+            " from out of melee position."
+                }));
+            }
+            casterPawn.Drawer.rotator.Face(thing.DrawPos);
+            if (!verb_MeleeAttack.Method("IsTargetImmobile", currentTarget).GetValue<bool>() && casterPawn.skills != null)
+            {
+                casterPawn.skills.Learn(SkillDefOf.Melee, 250f, false);
+            }
+            bool result;
+            SoundDef soundDef;
+            if (Rand.Value < verb_MeleeAttack.Method("GetHitChance", thing).GetValue<float>())
+            {
+                result = true;
+                verb_MeleeAttack.Method("ApplyMeleeDamageToTarget", currentTarget);
+                if (thing.def.category == ThingCategory.Building)
+                {
+                    soundDef = verb_MeleeAttack.Method("SoundHitBuilding").GetValue<SoundDef>();
+                }
+                else
+                {
+                    soundDef = verb_MeleeAttack.Method("SoundHitPawn").GetValue<SoundDef>();
+                }
+            }
+            else
+            {
+                result = false;
+                soundDef = verb_MeleeAttack.Method("SoundMiss").GetValue<SoundDef>();
+            }
+            soundDef.PlayOneShot(new TargetInfo(thing.Position, casterPawn.Map, false));
+            casterPawn.Drawer.Notify_MeleeAttackOn(thing);
+            Pawn pawn = thing as Pawn;
+            if (pawn != null && !pawn.Dead)
+            {
+                pawn.stances.StaggerFor(95);
+                if (casterPawn.MentalStateDef != MentalStateDefOf.SocialFighting || pawn.MentalStateDef != MentalStateDefOf.SocialFighting)
+                {
+                    pawn.mindState.meleeThreat = casterPawn;
+                    pawn.mindState.lastMeleeThreatHarmTick = Find.TickManager.TicksGame;
+                }
+            }
+            casterPawn.Drawer.rotator.FaceCell(thing.Position);
+            if (casterPawn.caller != null)
+            {
+                casterPawn.caller.Notify_DidMeleeAttack();
+            }
+            return result;
+        }
+    }
+}
