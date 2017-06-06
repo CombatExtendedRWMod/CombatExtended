f05fdd79f55c11bb5186358f0cab5abc0e56ce24
NoImageAvailable <meinekatzehatdickeeier@yahoo.com>
2017-06-06 16:01:25 +0200

    More detail for melee penetration
    Description now provides a breakdown by damage type

--- Source/CombatExtended/CombatExtended/StatWorkers/StatWorker_MeleeArmorPenetration.cs
+++ Source/CombatExtended/CombatExtended/StatWorkers/StatWorker_MeleeArmorPenetration.cs
@@ -10,25 +10,31 @@ namespace CombatExtended
 {
     public class StatWorker_MeleeArmorPenetration : StatWorker
     {
-        private float GetMeleePenetration(StatRequest req)
+        private float GetMeleePenetration(StatRequest req, DamageDef damageType = null)
         {
             Pawn pawn = req.Thing as Pawn;
             if (pawn == null)
             {
-                return 0f;
+                return 0;
             }
             List<VerbEntry> updatedAvailableVerbsList = pawn.meleeVerbs.GetUpdatedAvailableVerbsList();
             if (updatedAvailableVerbsList.Count == 0)
             {
-                return 0f;
+                return 0;
             }
+
+            List<VerbEntry> verbsList;
+            if (damageType != null) verbsList = updatedAvailableVerbsList.Where(x => x.verb.verbProps.meleeDamageDef == damageType).ToList();
+            else verbsList = updatedAvailableVerbsList;
+
+
             float totalSelectionWeight = 0f;
-            for (int i = 0; i < updatedAvailableVerbsList.Count; i++)
+            for (int i = 0; i < verbsList.Count; i++)
             {
-                totalSelectionWeight += updatedAvailableVerbsList[i].SelectionWeight;
+                totalSelectionWeight += verbsList[i].SelectionWeight;
             }
             float totalAveragePen = 0f;
-            foreach (VerbEntry current in updatedAvailableVerbsList)
+            foreach (VerbEntry current in verbsList)
             {
                 var propsCE = current.verb.verbProps as VerbPropertiesCE;
                 if (propsCE != null)
@@ -48,9 +54,33 @@ namespace CombatExtended
 
         public override string GetExplanation(StatRequest req, ToStringNumberSense numberSense)
         {
+            var pawn = req.Thing as Pawn;
+            if (pawn == null)
+                return base.GetExplanation(req, numberSense);
+
+            // Find all damage types of our verbs
+            List<DamageDef> allDamageTypes = new List<DamageDef>();
+            foreach(VerbEntry current in pawn.meleeVerbs.GetUpdatedAvailableVerbsList())
+            {
+                var dam = current.verb.verbProps.meleeDamageDef;
+                if (dam != null) allDamageTypes.Add(dam);
+            }
+
+            // Generate entries
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("CE_StatsReport_MeleePenetration".Translate() + " (" + "AverageOfAllAttacks".Translate() + ")");
-            stringBuilder.AppendLine("  " + GetMeleePenetration(req).ToString("0.##"));
+            foreach(DamageDef current in allDamageTypes.Distinct())
+            {
+                var averageString = "";
+                if (allDamageTypes.FindAll(x => x == current).Count > 1)
+                {
+                    averageString = " (" + "AverageOfAllAttacks".Translate() + ")";
+                }
+                stringBuilder.AppendLine(current.LabelCap + averageString);
+                stringBuilder.AppendLine("  " + GetMeleePenetration(req, current).ToString("0.##"));
+                stringBuilder.AppendLine();
+            }
+            stringBuilder.Length--; // Move back one position to remove last empty line
+
             return stringBuilder.ToString();
         }
     }
