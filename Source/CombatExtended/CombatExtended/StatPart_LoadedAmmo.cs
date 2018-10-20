1067e33b1307116ee3a0badac0022b447290283b
XeoNovaDan <toghill7252010@hotmail.co.uk>
2018-10-20 17:39:34 +0100

    Ammo no longer defies the laws of physics

---
+++ Source/CombatExtended/CombatExtended/StatPart_LoadedAmmo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace CombatExtended
+{
+    public class StatPart_LoadedAmmo : StatPart
+    {
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (req.HasThing && req.Thing.TryGetComp<CompAmmoUser>() is CompAmmoUser compAmmo && compAmmo.UseAmmo)
+            {
+                float valOffset = compAmmo.CurMagCount * compAmmo.CurrentAmmo.GetStatValueAbstract(StatDefOf.Mass);
+                return $"{"CE_LoadedAmmo".Translate()} : {parentStat.Worker.ValueToString(valOffset, false, ToStringNumberSense.Offset)}";
+            }
+            return null;
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (req.HasThing && req.Thing.TryGetComp<CompAmmoUser>() is CompAmmoUser compAmmo && compAmmo.UseAmmo)
+            {
+                val += compAmmo.CurMagCount * compAmmo.CurrentAmmo.GetStatValueAbstract(StatDefOf.Mass);
+            }
+        }
+    }
+}
